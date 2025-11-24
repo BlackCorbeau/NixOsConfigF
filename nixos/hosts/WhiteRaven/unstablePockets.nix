a30064806d50d638b4fde17a60fcf7176e0c6664
Kirill <remizov.kirill2005@yandex.ru>
2025-11-24 20:03:05 +0300

    fixup! fix(Packages): Delete unused packeges

---
+++ nixos/hosts/WhiteRaven/unstablePockets.nix
@@ -0,0 +1,124 @@
+{ pkgs, ... }:
+
+#let
+#  thingsboard = import ./pockets/thingsboard.nix { inherit pkgs; };
+#in
+{
+  nixpkgs.config = {
+    allowUnfree = true;
+    permittedInsecurePackages = ["python-2.7.18.12" "electron-25.9.0" "freeimage-unstable-2021-11-01" "obsidian-1.5.12"];
+  };
+
+  environment.systemPackages = with pkgs; [    
+    libreoffice
+    wget
+    home-manager
+    bat
+
+    #git
+    pkgs.git
+    lazygit
+    #terminals
+    ghostty
+
+    #VideoEditor
+    kdePackages.kdenlive
+
+    #basePrograms for work
+    pkgs.wofi
+    pkgs.google-chrome
+    tor-browser
+    grimblast
+    tree
+    slurp
+    fastfetch
+    mako
+    file
+    btop
+    nix-index
+    unzip
+    light
+    zip
+    brightnessctl
+    openssl
+    yazi
+
+    swww
+    gnumake
+
+    python2
+    (python3.withPackages (ps: with ps; [ requests bpython ]))
+    python311Packages.pip
+
+    #text editor
+    helix
+
+    # IDES
+    #jetbrains.idea-community
+
+    #Utiles
+    postman
+
+    # Code
+    zed-editor
+
+    #java
+    gradle
+    openjdk
+
+    #Kotlin
+    kotlin
+    android-studio
+    
+    #DB
+    postgresql
+    dbgate
+
+    # Bluetooth
+    bluez
+    bluez-tools
+
+    # Sounds
+    pipewire
+    pulseaudio
+    pamixer
+    cava
+
+    # obs-studio
+    obs-studio
+
+    #videoManager
+    vlc
+
+    #learning
+    zoom-us
+    obsidian
+
+    #Usb mouting and acsess
+    udisks
+
+    #virtualization
+    qemu
+    kdePackages.partitionmanager
+
+    #Torent
+    #torrential
+    deluge
+  ];
+
+  fonts.packages = with pkgs; [
+    jetbrains-mono
+    noto-fonts
+    (google-fonts.override{fonts=["Press Start 2P" "Overpass Mono"];})
+    noto-fonts-color-emoji
+    noto-fonts-cjk-sans
+    twemoji-color-font
+    font-awesome
+    powerline-fonts
+    powerline-symbols
+    ubuntu-classic
+    unifont
+    nerd-fonts.symbols-only
+    corefonts
+  ];
+}
